c7c36534d9864b39ba41768dbf35faf97587d81d
anlideer <anlideer@gmail.com>
2022-03-08 23:38:03 +0200

    for stat

---
+++ ASTComponents/DefStat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniPLCompiler.ASTComponents
+{
+    class DefStat : BaseNode
+    {
+        public override BaseNode TryBuild(ref Scanner scanner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ASTComponents/Statements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniPLCompiler.ASTComponents
+{
+    class Statements : BaseNode
+    {
+        public List<BaseNode> statsList = new List<BaseNode>();
+
+        public override BaseNode TryBuild(ref Scanner scanner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
